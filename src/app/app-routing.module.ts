e08ac38e54deb42873874c51e25d42ada8eae440
cliverFray <u20201b288@upc.edu.pe>
2023-04-24 17:57:28 -0500

    add styles component menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PaisComponent } from './component/pais/pais.component';
 import { PaisInsertarComponent } from './component/pais/pais-insertar/pais-insertar.component';
+import { HomeComponent } from './component/home/home.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
   {
     path: 'paises',
     component: PaisComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,8 +17,17 @@ import {MatInputModule} from '@angular/material/input';
 
 import {MatSelectModule} from '@angular/material/select';
 
-import { MatButtonModule } from '@angular/material/button';
 import { HomeComponent } from './component/home/home.component';
+
+//
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDialogModule } from '@angular/material/dialog';
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,7 +48,17 @@ import { HomeComponent } from './component/home/home.component';
     FormsModule,
     MatInputModule,
     MatSelectModule,
-    MatButtonModule
+    MatButtonModule,
+    LayoutModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule,
+
+
+
+    MatSidenavModule,
+    MatDividerModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
