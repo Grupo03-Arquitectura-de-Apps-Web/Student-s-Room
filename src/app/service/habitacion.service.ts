4cd00200f527549e95644412d2a0f03f41f4ec70
GiaGR7 <u20201a955@upc.edu.pe>
2023-06-15 11:58:00 -0500

    Se agregaron los token a los métodos de los siguientes servicios: universidad, habitacion y ciudad

--- src/app/service/habitacion.service.ts
+++ src/app/service/habitacion.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { habitacion } from '../model/habitacion';
 import { Subject } from 'rxjs';
@@ -17,11 +17,23 @@ export class HabitacionService {
   constructor(private http: HttpClient) {}
 
   list() {
-    return this.http.get<habitacion[]>(this.url);
+    let token = sessionStorage.getItem('token');
+
+    return this.http.get<habitacion[]>(this.url, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
   }
 
   insert(habitacion: habitacion) {
-    return this.http.post(this.url, habitacion);
+    let token = sessionStorage.getItem('token');
+
+    return this.http.post(this.url, habitacion, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
   }
 
   getList() {
@@ -33,15 +45,33 @@ export class HabitacionService {
   }
 
   listID(id: number) {
-    return this.http.get<habitacion>(`${this.url}/${id}`);
+    let token = sessionStorage.getItem('token');
+
+    return this.http.get<habitacion>(`${this.url}/${id}`, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
   }
 
   update(a: habitacion) {
-    return this.http.put(this.url + '/' + a.id, a);
+    let token = sessionStorage.getItem('token');
+
+    return this.http.put(this.url, a, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
   }
 
   eliminar(id: number) {
-    return this.http.delete(`${this.url}/${id}`);
+    let token = sessionStorage.getItem('token');
+
+    return this.http.delete(`${this.url}/${id}`, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
   }
   getConfirmaEliminacion() {
     return this.confirmaEliminacion.asObservable();
--- src/app/service/universidad.service.ts
+++ src/app/service/universidad.service.ts
@@ -1,53 +1,78 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Universidad } from '../model/universidad';
 import { Subject } from 'rxjs';
 
-
-const base_url=environment.base
+const base_url = environment.base;
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UniversidadService {
-  private url=`${base_url}/universidad`
-  private listaCambio = new Subject<Universidad[]>()
-  private confirmaEliminacion = new Subject<Boolean>()
+  private url = `${base_url}/universidad`;
+  private listaCambio = new Subject<Universidad[]>();
+  private confirmaEliminacion = new Subject<Boolean>();
 
+  constructor(private http: HttpClient) {}
+  list() {
+    let token = sessionStorage.getItem('token');
 
-  constructor(private http:HttpClient) {}
-    list(){
-      return this.http.get<Universidad[]>(this.url);
-    }
-    insert(Universidad: Universidad) {
-      return this.http.post(this.url, Universidad);
-    }
+    return this.http.get<Universidad[]>(this.url, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
+  }
+  insert(Universidad: Universidad) {
+    let token = sessionStorage.getItem('token');
 
-    getList() {
-      return this.listaCambio.asObservable();
-    }
-    setList(listaNueva: Universidad[]) {
-      this.listaCambio.next(listaNueva);
-    }
-
-    listID(id:number){
-      return this.http.get<Universidad>(`${this.url}/${id}`);
-    }
-
-    update(u:Universidad){
-      return this.http.put(this.url + '/' + u.id,u);
-    }
-
-    eliminar(id: number) {
-
-      return this.http.delete(`${this.url}/${id}`);
-    }
-    getConfirmaEliminacion() {
-      return this.confirmaEliminacion.asObservable();
-    }
-    setConfirmaEliminacion(estado: Boolean) {
-      this.confirmaEliminacion.next(estado);
-    }
+    return this.http.post(this.url, Universidad, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
   }
 
+  getList() {
+    return this.listaCambio.asObservable();
+  }
+  setList(listaNueva: Universidad[]) {
+    this.listaCambio.next(listaNueva);
+  }
 
+  listID(id: number) {
+    let token = sessionStorage.getItem('token');
+
+    return this.http.get<Universidad>(`${this.url}/${id}`, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
+  }
+
+  update(u: Universidad) {
+    let token = sessionStorage.getItem('token');
+
+    return this.http.put(this.url, u, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
+  }
+
+  eliminar(id: number) {
+    let token = sessionStorage.getItem('token');
+
+    return this.http.delete(`${this.url}/${id}`, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
+  }
+  getConfirmaEliminacion() {
+    return this.confirmaEliminacion.asObservable();
+  }
+  setConfirmaEliminacion(estado: Boolean) {
+    this.confirmaEliminacion.next(estado);
+  }
+}
